74796fb00d1cc8cd7a3e1711f80818631e400875
Bishoy Hany <bishoyhany31@hotmail.com>
2023-12-25 20:58:07 +0200

    Map GetAllProducts to return ProductDto

--- Demo1.APIs/Controllers/ProductsController.cs
+++ Demo1.APIs/Controllers/ProductsController.cs
@@ -1,9 +1,13 @@
-﻿using Demo1.Core.Entities;
+﻿using AutoMapper;
+using Demo1.APIs.Helpers.DTOs;
+using Demo1.Core.Entities;
 using Demo1.Core.Interfaces;
 using Demo1.Core.Interfaces.Repositories;
 using Demo1.Core.Specifications;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Demo1.APIs.Controllers
@@ -12,17 +16,21 @@ namespace Demo1.APIs.Controllers
 	public class ProductsController : BaseAPIController
 	{
 		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
 
-		public ProductsController(IUnitOfWork unitOfWork)
+		public ProductsController(IUnitOfWork unitOfWork, IMapper mapper)
 		{
 			this._unitOfWork = unitOfWork;
+			this._mapper = mapper;
 		}
 
 		[HttpGet]
 		public async Task<IActionResult> GetAllProducts()
 		{
 			var specs = new ProductWithTypeAndCategorySpecification();
-			return Ok(await _unitOfWork.ProductRepo.GetAllBySpecsAsync(specs));
+			var products = await _unitOfWork.ProductRepo.GetAllBySpecsAsync(specs);
+			var productsDto = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDto>>(products);
+			return Ok(productsDto);
 		}
 
 		[HttpGet("{id}")]
