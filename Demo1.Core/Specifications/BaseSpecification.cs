383a022c253a0a19998f631c7e4ff5198f5b3706
Bishoy Hany <bishoyhany31@hotmail.com>
2023-12-25 20:37:19 +0200

    Implement Specification Design Pattern & Specification Evaluator

---
+++ Demo1.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,27 @@
+﻿using Demo1.Core.Entities;
+using Demo1.Core.Interfaces.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Core.Specifications
+{
+	public class BaseSpecification<T> : ISpecification<T> where T : BaseEntity
+	{
+		public Expression<Func<T, bool>> Criteria { get; set; }
+		public List<Expression<Func<T, object>>> IncludesList { get; set; } = new List<Expression<Func<T, object>>>();
+
+		public BaseSpecification()
+		{
+
+		}
+
+        public BaseSpecification(Expression<Func<T, bool>> _criteria)
+        {
+			Criteria = _criteria;
+        }
+    }
+}
---
+++ Demo1.Repository/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using Demo1.Core.Entities;
+using Demo1.Core.Interfaces.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Repository
+{
+	public class SpecificationEvaluator<T>  where T : BaseEntity
+	{
+		public IQueryable<T> BuildQuery(IQueryable<T> mainQuery, ISpecification<T> specs)
+		{
+			var GeneratedQuery = mainQuery;
+
+			if(specs.Criteria != null)
+				GeneratedQuery = GeneratedQuery.Where(specs.Criteria);
+
+			GeneratedQuery = specs.IncludesList.Aggregate(GeneratedQuery, (query, exp) => query.Include(exp));
+
+			return GeneratedQuery;
+		}
+	}
+}
